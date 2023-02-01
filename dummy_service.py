a8784c493e00f03c08397b925b582f43bd0a7289
tombola <thomas.readings@gmail.com>
2023-02-01 11:09:17 +0000

    Test if responses will return if a querystring is added to url

--- dummy_service.py
+++ dummy_service.py
@@ -3,3 +3,4 @@ from dummy_api import PRODUCTS_API_URL
 
 def get_products():
     return requests.get(PRODUCTS_API_URL)
+
--- tests/test_responses_mocks.py
+++ tests/test_responses_mocks.py
@@ -2,21 +2,34 @@
 import pytest
 import responses
 import tomllib as _toml  # type: ignore[no-redef]
-from responses import _recorder
-from responses.matchers import fragment_identifier_matcher
+
 from rich import print
 from utils import explore
 
 from dummy_api import API_URL, PRODUCTS_API_URL
 import dummy_service as service
+import requests
+
+@pytest.mark.dummy
+@responses.activate
+def test_mocked_response():
+    responses.patch(API_URL)
+    responses._add_from_file(file_path="recorded_responses/test_responses_recording.toml")
+
+    response = service.get_products()
+    explore(response)
+    print(response.json())
+
+    assert response.status_code == 200
 
 
 @pytest.mark.dummy
 @responses.activate
-def test_responses_recording():
+def test_mocked_response_with_arbitrary_query():
     responses.patch(API_URL)
     responses._add_from_file(file_path="recorded_responses/test_responses_recording.toml")
-    response = service.get_products()
+
+    response = requests.get(PRODUCTS_API_URL + "?q=2&something")
     explore(response)
     print(response.json())
 
